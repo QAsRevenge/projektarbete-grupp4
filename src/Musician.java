a621f97083e1fdd7896821de76bf2b8b090c572c
draginojd <arminfazlikhan@gmail.com>
2022-09-30 17:02:39 +0200

    First i added a toString to the Album class, then after i created anand ArrayList <Album>to Musician.java. I also created a method called addMusicianToAlbum wich has no uses yet.

--- src/Musician.java
+++ src/Musician.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Musician {
-    Scanner in = new Scanner(System.in);
+
     // Fields specific to musicians
     public String firstName;
     public String lastName;
@@ -12,27 +11,30 @@ public class Musician {
     public String instrument;
     public ArrayList<Band> bands = new ArrayList<>();
 public ArrayList<Musician> musicians = new ArrayList<>();
+ public ArrayList<Album> albums = new ArrayList<>();
 
 
 
     // Constructor
 
-    public Musician(String firstName, String lastName, String infoText, String dateOfBirth, String dateOfDeath, String instrument){
+    public Musician (String firstName, String lastName, String infoText, String dateOfBirth, String dateOfDeath, String instrument){
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.dateOfDeath = dateOfDeath;
         this.infoText = infoText;
         this.instrument = instrument;
-        Musician userCreateMusician = new Musician(firstName, lastName, infoText, dateOfBirth, dateOfDeath, instrument);
     }
 
 
     public String toString(){
         StringBuilder about = new StringBuilder("Name: " + firstName + lastName + "\nDate of birth: " + dateOfBirth + "\nDate of death: " + dateOfDeath +"\nAbout the musician: " + infoText + "\nInstrument the musician is playing: " + instrument);
-        return about + ""; //+firstName + " " + lastName + ": " + dateOfBirth + ": " +dateOfDeath + ": " + infoText + ": " + instrument;
+        return about + "";
     }
 
+    public void addMusicianToAlbum(){
+
+    }
     public void joinBand(Band bandToJoin){
         if (!bands.contains(bandToJoin)){
             bands.add(bandToJoin);
