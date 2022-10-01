597799ea07005239a3c26dd3d8876a24f7c586d9
draginojd <arminfazlikhan@gmail.com>
2022-10-01 20:28:30 +0200

    in Musician.java I have created these methods called:  addMusicianByName,  searchMusicianByName,  showMusicianList and  removeMusicianByName. They are not 100% finished yet! In the Main.java I have made a sketch of how I want the nested switchcase for Musician to look alike.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Main {
+
+
     public static void main(String[] args) {
         menu("Pick a number between 1-3.\nChoice 1: Add a Band\nChoice 2: Add a Musician\nChoice 3: Add a Album");
     }
@@ -35,7 +37,26 @@ public class Main {
                 System.out.println("Wich instrument does the musician use? answer:");
                 String instrument = in.nextLine();
                 Musician userCreateMusician = new Musician(firstName, lastName, dateOfBirth, dateOfDeath, infoText, instrument);
-                System.out.println(userCreateMusician);
+                System.out.println(userCreateMusician + "\n");
+//_____________________________________________________________________________________________
+                //nested case: add another musician
+                // System.out.println("Add another musician\n");
+                userCreateMusician.addMusician(userCreateMusician);
+
+                //nested case: show musician list
+             userCreateMusician.showMusicianList();
+                System.out.println();
+                //nested case: search MusicianByName
+                userCreateMusician.searchMusicianByName(userCreateMusician.firstName, userCreateMusician.lastName);
+                //nested case: remove musician by name
+             userCreateMusician.removeMusicianByName(userCreateMusician.getFirstName());
+                System.out.println();
+                userCreateMusician.showMusicianList();
+
+                //Ta inte bort
+//______________________________________________________________________________________________
+
+
               /*  switch (parsedmenuNumber){
                 System.out.println("Pick between one of the following\n1. Add another musician");
 
--- src/Musician.java
+++ src/Musician.java
@@ -10,13 +10,10 @@ public class Musician {
     public String dateOfDeath;
     public String instrument;
     public ArrayList<Band> bands = new ArrayList<>();
-public ArrayList<Musician> musicians = new ArrayList<>();
  public ArrayList<Album> albums = new ArrayList<>();
-
-
+    public ArrayList<Musician> musicianList = new ArrayList<>();
 
     // Constructor
-
     public Musician (String firstName, String lastName, String infoText, String dateOfBirth, String dateOfDeath, String instrument){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -26,15 +23,85 @@ public ArrayList<Musician> musicians = new ArrayList<>();
         this.instrument = instrument;
     }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void addMusician(Musician musician){
+        musicianList.add(musician);
+    }
+    public void removeMusicianByName(String firstName){
+        for (Musician musician: musicianList) {
+            if (musician.getFirstName() + musician.getLastName() == firstName + " " + lastName){
+                musicianList.remove(musician);
+            }
+        }
+        showMusicianList();
+    }
+
+
+    public void searchMusicianByName(String firstName, String lastName){
+        for (Musician musician: musicianList){
+            if ((musician.getFirstName().equals(firstName + " " + lastName))) {
+                System.out.println("Information about: " + musician.getFirstName() );
+                System.out.println(musician.getFirstName() + " " + musician.getLastName() + " " + infoText);
+
+            }
+        }
+    }
+    public void showMusicianList(){
+        for (Musician musician: musicianList){
+    System.out.println(musician.getFirstName() + " " + musician.getLastName() + " " + musician.getDateOfBirth() );
+}
+    }
+    //setters and getters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+    public void setDateOfDeath(String dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
+    public String getInfoText() {
+        return infoText;
+    }
+
+    public void setInfoText(String infoText) {
+        this.infoText = infoText;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
 
     public String toString(){
         StringBuilder about = new StringBuilder("Name: " + firstName + lastName + "\nDate of birth: " + dateOfBirth + "\nDate of death: " + dateOfDeath +"\nAbout the musician: " + infoText + "\nInstrument the musician is playing: " + instrument);
         return about + "";
     }
 
-    public void addMusicianToAlbum(){
-
-    }
     public void joinBand(Band bandToJoin){
         if (!bands.contains(bandToJoin)){
             bands.add(bandToJoin);
