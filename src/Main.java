2a82511a6f84564ac0e6f34521d1b81fa64ef27c
draginojd <arminfazlikhan@gmail.com>
2022-10-05 17:51:26 +0200

    I added a remove musician that currently do not work. But I will fix that later. I also did put a little bit more detail in some of the souts, and fixed the choice order.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         while (true) { //Display menu while true.
-            menu("Pick a number between 1-3.\nChoice 1: Add a Band.\nChoice 2: Add a Musician.\nChoice 3: Show already existing musicians.\nChoice 4: Delete an existing musician.\n");
+            menu("Pick a number between 1-3.\nChoice 1: Add a Band.\nChoice 2: Add a Musician.\nChoice 3: Show already existing musicians.\nChoice 4: Delete an existing musician.\nChoice quit: Exit the program by entering the word 'quit'. Remember to only use lowercases.");
         }
     }
 
