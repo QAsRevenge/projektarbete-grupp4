a5b185026d9b4d2ea6959ba9c1d008e4814c9852
draginojd <arminfazlikhan@gmail.com>
2022-10-05 15:46:10 +0200

    Created a while loop for the switch-case in the main in Main.java so while true the menu displays. I also deleted the nested case. we have to make more cases instead.

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,17 @@ public class Main {
     static ArrayList<Musician> musicians = new ArrayList<>();
 
     public static void main(String[] args) {
-        menu("Pick a number between 1-3.\nChoice 1: Add a Band\nChoice 2: Add a Musician\nChoice 3: Add a Album");
+        while (true) { //Display menu while true.
+            menu("Pick a number between 1-3.\nChoice 1: Add a Band\nChoice 2: Add a Musician\nChoice 3: Show already existing musicians");
+        }
     }
 
     public static void menu(String menuDisplay) {
         Scanner in = new Scanner(System.in);
-        String menu;
         System.out.println(menuDisplay);
 
         switch (in.nextLine()) {
-            case "1" -> {
-                System.out.println("You picked to add a Band");
-
-            }
+            case "1" -> System.out.println("You picked to add a Band");
             //Band;
             case "2" -> {
                 System.out.println("You picked to add a Musician. Great!");
@@ -37,55 +35,12 @@ public class Main {
                 Musician userCreateMusician = new Musician(firstName, lastName, dateOfBirth, dateOfDeath, infoText, instrument);
                 musicians.add(userCreateMusician);
                 System.out.println(userCreateMusician + "\n");
-                System.out.println("Choose between the following alternatives:\n 1. Add another musician.\n 2. Show existing musicians.");
-                switch (in.nextLine()) {
-                    case "1" -> {
 
-                    }
-                    case "2" -> {
-
-                    }
-                    case "3" -> {
-
-                    }
-                }
-
-
-                System.out.println(musicians);
-
-//_____________________________________________________________________________________________
-                //nested case: add another musician
-                // System.out.println("Add another musician\n");
-                //userCreateMusician.addMusician(userCreateMusician);
-
-                //nested case: show musician list
-                //   userCreateMusician.showMusicianList();
-                System.out.println();
-                //nested case: search MusicianByName
-                //   userCreateMusician.searchMusicianByName(userCreateMusician.firstName, userCreateMusician.lastName);
-                //nested case: remove musician by name
-                // userCreateMusician.removeMusicianByName(userCreateMusician.getFirstName());
-                System.out.println();
-                //    userCreateMusician.showMusicianList();
-
-                //Ta inte bort
-//______________________________________________________________________________________________
-
-
-              /*  switch (parsedMenuNumber){
-                System.out.println("Pick between one of the following\n1. Add another musician");
-                    case "add" -> System.out.println("add new musician");
-                    break;
-                    case "remove" -> System.out.println("remove existing an existing musician");
-                    break;
-                    case "all" -> System.out.println("show all existing musicians");
-                    default -> System.out.println("Pick one of the following numbers!");
-                } */
-                //Musician;
             }
-            case "3" -> System.out.println("You picked to add a Album");
+            case "3" -> System.out.println(musicians);
+            case "6" -> System.out.println("You picked to add a Album");
 
-
+            case "quit" -> System.exit(1);
             //Album;
             default -> menu("Not a valid input");
         }
