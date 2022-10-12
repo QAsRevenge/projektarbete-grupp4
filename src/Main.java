98866fddc4efc60104b6025cc53099fcb8a515b4
QAsRevenge <hampus.e.molin@gmail.com>
2022-10-12 02:47:48 +0200

    Added current options to menu that is displayed to the user.

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     static ArrayList<Album> albums = new ArrayList<>();
     public static void main(String[] args) {
         while (true) { //Display menu while true.
-            Menu.menu("Pick an option!\n1. Add a Band.\n2. Show all bands.\n3. Delete an already existing band.\n4. Add a musician.\n5. Show all musicians.\n6. Delete an already existing musician..\n7. Quit: Exit the program by entering the word 'quit'.");
+            Menu.menu("Pick an option!\n1. Add a Band.\n2. Show all bands.\n3. Delete an already existing band.\n4. Add a musician.\n5. Show all musicians.\n6. Delete an already existing musician.\n7. Add an album.\n8. Show all albums.\n9. Remove an already existing album.\n Quit: Exit the program by entering the word 'quit'.");
         }
     }
 }
