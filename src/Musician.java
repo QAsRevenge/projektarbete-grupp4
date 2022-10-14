60cc8ba15394a06afc4f8c7e1d426ee83ace2d41
zompes <simonisvedala@hotmail.com>
2022-10-14 21:26:54 +0200

    musicianAge metod in Musician.java. Currently is not used.

--- src/Musician.java
+++ src/Musician.java
@@ -1,5 +1,6 @@
 import com.google.gson.annotations.JsonAdapter;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Musician extends Item{
 
@@ -67,18 +68,30 @@ public class Musician extends Item{
             albumToRemove.removeMusician(this);
         }
         bands.remove(albumToRemove);
-
     }
-
-
-
 //Musician leave band //DO NOT MODIFY THIS METHOD UNDER! If you mess it up its on you.
     public void leaveBand(Band bandToRemove){
        if (bandToRemove.musicians.contains(this)){
         bandToRemove.removeMusician(this);
         }
        bands.remove(bandToRemove);
-
+    }
+    public void musicianAge(){
+        // If the dateOfDeath is skipped (as in an empty string)
+        // it should go in to the if statement directly below.
+        // Otherwise, it should go in to the else statement.
+        // Currently, does not show up in the musicians list, needs to be added.
+        if (Objects.equals(dateOfDeath, "")){
+            int currentYear = 2022;
+            int dateOfBirthInt = Integer.parseInt(dateOfBirth);
+            int musiciansAge = currentYear - dateOfBirthInt;
+            String musiciansAgeString = String.valueOf(musiciansAge);
+        } else {
+            int dateOfBirthInt = Integer.parseInt(dateOfBirth);
+            int dateOfDeathInt = Integer.parseInt(dateOfDeath);
+            int musiciansAge = dateOfDeathInt - dateOfBirthInt;
+            String musiciansAgeString = String.valueOf(musiciansAge);
+        }
     }
 }
 
